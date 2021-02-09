5c0134198197507e40d4074ae53edad6c93c7883
minabe <minabe@neopa.jp>
2021-02-09 19:03:12 +0900

    ファーストコミット

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          ログイン前のページです。
+        </v-card-title>
+        <form action="" method="post">
+          <label>
+            メールアドレス
+            <input type="email" name="mail">
+          </label><br>
+          <label>
+            パスワード
+            <input type="password" name="pass">
+          </label><br>
+          <input type="submit" name="送信">
+        </form>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
